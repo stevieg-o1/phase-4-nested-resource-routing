aaa46cf132b6e76629f03c88a0222b2f606d80bc
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-06-27 15:09:10 +0300

    attempt one

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,12 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
     render json: reviews, include: :dog_house
   end
 
